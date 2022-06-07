3fce327746fb4887e9364f433a34c45cdcf8f8be
Doug Leung <dleung46@mylangara.ca>
2022-06-07 11:49:00 -0700

    webpack modification

--- src/recommendWine.js
+++ src/recommendWine.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
-import "dotenv/config";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const RecommendWine = props => {
 
